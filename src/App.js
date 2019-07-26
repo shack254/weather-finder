5b3bfad881307aa074c9f1b20fb157448634ffb5
shack254 <123meshack@gmail.com>
2019-07-26 16:32:55 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Form from "./componets/form";
+import Title from "./componets/title";
+import Weather from "./componets/weather";
+const API_KEY = "3b45345c24616a04b0f47a8513f6a374";
+
+class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const API_calls = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`
+    );
+    const data = await API_calls.json();
+    if (city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter value"
+      });
+    }
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div className="split left">
+          <Title />
+        </div>
+        <div className="split right">
+          <Form getWeather={this.getWeather} />
+          <Weather
+            temperature={this.state.temperature}
+            city={this.state.city}
+            country={this.state.country}
+            humidity={this.state.humidity}
+            description={this.state.description}
+            error={this.state.error}
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/componets/form.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class Form extends Component {
+  render() {
+    return (
+      <form onSubmit={this.props.getWeather}>
+        <input type="text" name="city" placeholder="city..." />
+        <input type="text" name="country" placeholder="country..." />
+        <button>get weather</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
